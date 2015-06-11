4705b56353cfed9346aaaa9a8bc6bdd49c28141b
Dmitry Salnikov <mitya@Dmitrys-MacBook-Pro.local>
2015-06-11 17:50:38 +0300

    added login VC; compilation succeeded

---
+++ DribbbleSDKDemo/DribbbleSDKDemo/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  DribbbleSDKDemo
+//
+//  Created by Dmitry Salnikov on 6/11/15.
+//  Copyright (c) 2015 Agilie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ DribbbleSDKDemo/DribbbleSDKDemo/LoginViewController.m
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.m
+//  DribbbleSDKDemo
+//
+//  Created by Dmitry Salnikov on 6/11/15.
+//  Copyright (c) 2015 Agilie. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
