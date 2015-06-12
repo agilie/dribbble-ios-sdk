9ae4018a4804a06d6ba56ee8b8b73a2d0a37d0d0
kalamaznik <zgonikvova@gmail.com>
2015-06-12 12:34:39 +0300

    pod update and sdk fixes

---
+++ DribbbleSDK Tests/DribbbleSDK Tests/DRLoadDataTestCase.h
@@ -0,0 +1,13 @@
+//
+//  DRLOadDataTestCase.h
+//  DribbbleSDK Tests
+//
+//  Created by zgonik vova on 12.06.15.
+//
+//
+
+#ifndef DribbbleSDK_Tests_DRLOadDataTestCase_h
+#define DribbbleSDK_Tests_DRLOadDataTestCase_h
+
+
+#endif
