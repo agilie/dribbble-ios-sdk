2795a27fe8bcb759a1168946722992641c99f13f
kalamaznik <zgonikvova@gmail.com>
2015-06-22 13:32:09 +0300

    remove HUD handlers and minor fefactor

--- DribbbleSDK/Client/DRApiClient.m
+++ DribbbleSDK/Client/DRApiClient.m
@@ -139,7 +139,7 @@ void logInteral(NSString *format, ...) {
         }
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
 
-        if (self.clientErrorHandler) self.clientErrorHandler(error);
+        if (weakSelf.clientErrorHandler) weakSelf.clientErrorHandler(error);
         if (completionHandler) completionHandler([DRBaseModel modelWithError:error]);
     }];
     return operation;
